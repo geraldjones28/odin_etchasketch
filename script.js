b7ca94a4c74ed242a3a9d10fa1b69675fd8d5985
GeraldJones <gajone12@asu.edu>
2024-03-04 14:54:25 -0800

    feat: Add a 16x16 grid using flexbox

---
+++ script.js
@@ -0,0 +1,6 @@
+const containerDiv = document.querySelector(".grid-container");
+
+for (let j = 1; j <= 273; j++) {
+    const newDiv = document.createElement("div");
+    containerDiv.append(newDiv);
+}
