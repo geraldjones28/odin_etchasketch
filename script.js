7beb9da69d77c0211c6883bb7a684a2c864acca8
GeraldJones <gajone12@asu.edu>
2024-03-04 18:36:01 -0800

    feat: Add functionable click-drag feature & separated randomColor generator

--- script.js
+++ script.js
@@ -8,21 +8,25 @@ for (let j = 1; j <= 273; j++) {
     containerDiv.append(newDiv);
 }
 
-/* Generates a random color as mouse hovers over squares */
+/* Generates a random color as mouse click-drags over squares */
 for (let i = 0 ; i < boxes.length; i++) {
+    boxes[i].addEventListener('mousedown', () => {
+        boxes[i].style.backgroundColor = getRandomColor();
+    }); 
     boxes[i].addEventListener('mouseenter', (e) => {
-        let c1 = Math.floor(Math.random() * (150 - 120) + 120);
-        let c2 = Math.floor(Math.random() * (150 - 120) + 120);
-        let c3 = Math.floor(Math.random() * (150 - 120) + 120);
-
-        boxes[i].style.backgroundColor = 
-        "rgb("+c1+","+c2+","+c3+")";
-        console.log(c1 + " " + c2 + " " + c3);
+        if (e.buttons === 1) {
+            boxes[i].style.backgroundColor = getRandomColor();
+        }
     }); 
 }
 
-function toggle() {
+/* Generate a random color */
+function getRandomColor() {
+    let c1 = Math.floor(Math.random() * (150 - 120) + 120);
+    let c2 = Math.floor(Math.random() * (150 - 120) + 120);
+    let c3 = Math.floor(Math.random() * (150 - 120) + 120);
 
+    return "rgb("+c1+","+c2+","+c3+")";
 }
 
 /* Refreshes the page to give a clean grid */
