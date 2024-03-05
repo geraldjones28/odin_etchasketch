297cb894c1115cc0410c81af4510eba77472ba72
GeraldJones <gajone12@asu.edu>
2024-03-04 17:53:42 -0800

    feat: Individual box mouseover functionality works. Not optimal, but works.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const containerDiv = document.querySelector(".grid-container");
+const boxes = document.getElementsByClassName('box');
 
 /* Adds 273 divs to create an even 16x16 grid */
 for (let j = 1; j <= 273; j++) {
     const newDiv = document.createElement("div");
-    newDiv.className = "color";
+    newDiv.setAttribute('class', 'box');
     containerDiv.append(newDiv);
 }
 
-/* Working on changing color feature when mouse hovers over div */
-containerDiv.addEventListener('mousemove', (e) => {
-    containerDiv.style.backgroundColor = 
-    "rgb("+e.offsetX+","+e.offsetY+", 40)";
-    console.log('x:' + e.offsetX + ", y:" + e.offsetY);
-});
+for (let i = 0 ; i < boxes.length; i++) {
+    boxes[i].addEventListener('mouseover', (e) => {
+        boxes[i].style.backgroundColor = 
+        "rgb("+e.offsetX+","+e.offsetY+", 40)";
+        console.log('x:' + e.offsetX + ", y:" + e.offsetY);
+    }) ; 
+}
