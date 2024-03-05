1686b224fb96aeb9bfd3ab4e6b0c2fd20e1f1bd7
GeraldJones <gajone12@asu.edu>
2024-03-05 08:43:25 -0800

    feat: Working on dynamically creating squares within grid that are automatically given equal heights/widths

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
 const containerDiv = document.querySelector(".grid-container");
-const boxes = document.getElementsByClassName('col-boxes');
-const row = document.getElementById('row');
-const col = document.getElementById('col');
+const row = document.getElementsByClassName('row');
+const square = document.getElementsByClassName('square');
+row.textContent = 16;
+square.textContent = 16;
 makeGrid();
 
 function changeSize() {
-
+    resetGrid();
+    const num = prompt(
+        "Change the size of the board (resolution). Min: 16 Max: 100"
+    );
+    if ((num >= 16) && (num <= 100)) {
+        row.textContent = num;
+        square.textContent = num;
+        makeGrid();
+    } else {
+        makeGrid();
+    }
 }
 
 /* Adds divs to DOM inputted by user (TBA) to create an even grid */
 function makeGrid() {
     for (let i = 1; i <= row.textContent; i++) {
         const rowDiv = document.createElement("div");
-        rowDiv.setAttribute('class', 'row-boxes');
+        rowDiv.setAttribute('class', 'row');
         containerDiv.append(rowDiv);
-        for (let j = 1; j <= col.textContent; j++) {
-            const colDiv = document.createElement("div");
-            colDiv.setAttribute('class', 'col-boxes');
-            rowDiv.append(colDiv);
+        for (let j = 1; j <= square.textContent; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.setAttribute('class', 'square');
+            squareDiv.style.height = `${500 / row}px`;
+            squareDiv.style.width = `${500 / row}px`;
+            rowDiv.append(squareDiv);
         }
     }
+    mouseEvents();
+}
+/* Refreshes grid pre changeSize() function */
+function resetGrid() {
+    const squareRows = document.querySelectorAll(".row");
+    squareRows.forEach((row) => {
+    containerDiv.removeChild(row);
+    mouseEvents();
+  });
 }
 
-/* Gets the random color and shows it as mouse click-drags over squares */
-for (let i = 0 ; i < boxes.length; i++) {
-    boxes[i].addEventListener('mousedown', () => {
-        boxes[i].style.backgroundColor = getRandomColor();
-    }); 
-    boxes[i].addEventListener('mouseenter', (e) => {
-        if (e.buttons === 1) {
-            boxes[i].style.backgroundColor = getRandomColor();
-        }
-    }); 
+/* Gets the random squareor and shows it as mouse click-drags over squares */
+function mouseEvents() {
+    for (let i = 0 ; i < square.length; i++) {
+        square[i].addEventListener('mousedown', () => {
+            square[i].style.backgroundColor = getRandomColor();
+        }); 
+        square[i].addEventListener('mouseenter', (e) => {
+            if (e.buttons === 1) {
+                square[i].style.backgroundColor = getRandomColor();
+            }
+        }); 
+    }
 }
 /* Generate a random color */
 function getRandomColor() {
