d9abf30e077be395c374f59627c04e6f9590051e
GeraldJones <gajone12@asu.edu>
2024-03-04 18:10:51 -0800

    feat: Add refresh page functionality

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ for (let j = 1; j <= 273; j++) {
     containerDiv.append(newDiv);
 }
 
+/* Generates a random color as mouse hovers over squares */
 for (let i = 0 ; i < boxes.length; i++) {
     boxes[i].addEventListener('mouseover', (e) => {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+
         boxes[i].style.backgroundColor = 
-        "rgb("+e.offsetX+","+e.offsetY+", 40)";
-        console.log('x:' + e.offsetX + ", y:" + e.offsetY);
+        "rgb("+red+","+green+","+blue+")";
     }) ; 
+}
+
+/* Refreshes the page to give a clean grid */
+function resetGame() {
+    location.reload();
 }
